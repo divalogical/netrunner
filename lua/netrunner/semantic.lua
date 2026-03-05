e314a81ba58c3f5d52380c39b3c9c6e45dfd5ad8
divalogical <dev@divalogical.vip>
2026-03-05 13:16:28 -0800

    Semantic.lua

--- lua/netrunner/semantic.lua
+++ lua/netrunner/semantic.lua
@@ -1,7 +1,6 @@
 return function(p)
 
   return {
-
     bg = p.bg,
     fg = p.fg,
 
@@ -10,17 +9,17 @@ return function(p)
 
     comment = p.green,
 
-    keyword = p.green,
+    keyword = p.red,
     function_name = p.green,
-    string = p.green,
+    string = p.cyan,
     number = p.green,
-    type = p.green,
+    type = p.blue,
 
-    error = p.green,
+    error = p.red,
     warning = p.green,
     hint = p.green,
 
-    accent = p.green
+    accent = p.green,
   }
 
 end
